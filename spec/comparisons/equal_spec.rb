98aff98111c2a95ef487761ab8f37c7c31ae3c2c
Eugene Westbrook <eugenewestbrook@gmail.com>
2015-07-20 16:31:27 -0400

    Refactors Equal and GreaterThan to use value_for

--- spec/comparisons/equal_spec.rb
+++ spec/comparisons/equal_spec.rb
@@ -1,30 +1,56 @@
 Eugeneral.require_sub_module('comparisons')
 
 describe Eugeneral::Comparisons::Equal do
-  let(:subject) { double(:subject) }
-  let(:object) { double(:object) }
-  let(:comparator) { described_class.new(subject, object) }
+  context 'Given an Equal Comparitor' do
+    let(:comparator) { described_class.new(subject, object) }
 
-  before do
-    allow(subject).to receive(:resolve).and_return(expected_value)
-    allow(object).to receive(:resolve).and_return(actual_value)
-  end
+    context 'instantiated with commands' do
+      let(:subject) { double(:subject) }
+      let(:object) { double(:object) }
 
-  context 'When subject and object values are equal' do
-    let(:actual_value) { :foo }
-    let(:expected_value) { :foo }
+      before do
+        allow(subject).to receive(:resolve).and_return(subject_value)
+        allow(object).to receive(:resolve).and_return(object_value)
+      end
 
-    it 'resolves to true' do
-      expect(comparator.resolve('anything')).to eq(true)
+      context 'When subject and object values are equal' do
+        let(:object_value) { :foo }
+        let(:subject_value) { :foo }
+
+        it 'resolves to true' do
+          expect(comparator.resolve('anything')).to eq(true)
+        end
+      end
+
+      context 'When subject and object values are not equal' do
+        let(:object_value) { :foo }
+        let(:subject_value) { :bar }
+
+        it 'resolves to true' do
+          expect(comparator.resolve('anything')).to eq(false)
+        end
+      end
     end
-  end
 
-  context 'When subject and object values are not equal' do
-    let(:actual_value) { :foo }
-    let(:expected_value) { :bar }
+    context 'instantiated with values' do
 
-    it 'resolves to true' do
-      expect(comparator.resolve('anything')).to eq(false)
+      context 'When subject and object values are equal' do
+        let(:object) { :foo }
+        let(:subject) { :foo }
+
+        it 'resolves to true' do
+          expect(comparator.resolve('anything')).to eq(true)
+        end
+      end
+
+      context 'When subject and object values are not equal' do
+        let(:object) { :foo }
+        let(:subject) { :bar }
+
+        it 'resolves to true' do
+          expect(comparator.resolve('anything')).to eq(false)
+        end
+      end
     end
   end
 end
--- spec/comparisons/greater_than_spec.rb
+++ spec/comparisons/greater_than_spec.rb
@@ -2,40 +2,142 @@ Eugeneral.require_sub_module('comparisons')
 
 describe Eugeneral::Comparisons::GreaterThan do
   context 'Given a comparison of GreaterThan' do
-    let(:subject) { double(:subject) }
-    let(:object) { double(:object) }
     let(:comparator) { described_class.new(subject, object) }
-    let(:args) { ['foo', 'bar'] }
 
-    before do
-      allow(subject).to receive(:resolve).with(*args).and_return(subject_value)
-      allow(object).to receive(:resolve).with(*args).and_return(object_value)
-    end
+    context 'when instantiated with commands' do
+      let(:subject) { double(:subject) }
+      let(:object) { double(:object) }
 
-    context 'when subject is greater than object' do
-      let(:subject_value) { 2 }
-      let(:object_value) { 1 }
+      context 'when resolved with args' do
+        let(:args) { ['foo', 'bar'] }
 
-      it 'resolves to true' do
-        expect(comparator.resolve(*args)).to eq(true)
+        before do
+          allow(subject).to receive(:resolve).with(args).and_return(subject_value)
+          allow(object).to receive(:resolve).with(args).and_return(object_value)
+        end
+
+        context 'when subject is greater than object' do
+          let(:subject_value) { 2 }
+          let(:object_value) { 1 }
+
+          it 'resolves to true' do
+            expect(comparator.resolve(args)).to eq(true)
+          end
+        end
+
+        context 'subject is less than object' do
+          let(:subject_value) { 1 }
+          let(:object_value) { 2 }
+
+          it 'resolves to false' do
+            expect(comparator.resolve(args)).to eq(false)
+          end
+        end
+
+        context 'when subject is equal to object' do
+          let(:subject_value) { 1 }
+          let(:object_value) { 1 }
+
+          it 'resolves to false' do
+            expect(comparator.resolve(args)).to eq(false)
+          end
+        end
+      end
+      context 'when resolved without args' do
+
+        before do
+          allow(subject).to receive(:resolve).and_return(subject_value)
+          allow(object).to receive(:resolve).and_return(object_value)
+        end
+
+        context 'when subject is greater than object' do
+          let(:subject_value) { 2 }
+          let(:object_value) { 1 }
+
+          it 'resolves to true' do
+            expect(comparator.resolve).to eq(true)
+          end
+        end
+
+        context 'subject is less than object' do
+          let(:subject_value) { 1 }
+          let(:object_value) { 2 }
+
+          it 'resolves to false' do
+            expect(comparator.resolve).to eq(false)
+          end
+        end
+
+        context 'when subject is equal to object' do
+          let(:subject_value) { 1 }
+          let(:object_value) { 1 }
+
+          it 'resolves to false' do
+            expect(comparator.resolve).to eq(false)
+          end
+        end
       end
     end
 
-    context 'subject is less than object' do
-      let(:subject_value) { 1 }
-      let(:object_value) { 2 }
+    context 'when instantiated with values' do
+      context 'when resolved with args' do
+        let(:args) { ['foo', 'bar'] }
 
-      it 'resolves to false' do
-        expect(comparator.resolve(*args)).to eq(false)
+        context 'when subject is greater than object' do
+          let(:subject) { 2 }
+          let(:object) { 1 }
+
+          it 'resolves to true' do
+            expect(comparator.resolve(args)).to eq(true)
+          end
+        end
+
+        context 'subject is less than object' do
+          let(:subject) { 1 }
+          let(:object) { 2 }
+
+          it 'resolves to false' do
+            expect(comparator.resolve(args)).to eq(false)
+          end
+        end
+
+        context 'when subject is equal to object' do
+          let(:subject) { 1 }
+          let(:object) { 1 }
+
+          it 'resolves to false' do
+            expect(comparator.resolve(args)).to eq(false)
+          end
+        end
       end
-    end
+      context 'when resolved without args' do
 
-    context 'when subject is equal to object' do
-      let(:subject_value) { 1 }
-      let(:object_value) { 1 }
+        context 'when subject is greater than object' do
+          let(:subject) { 2 }
+          let(:object) { 1 }
 
-      it 'resolves to false' do
-        expect(comparator.resolve(*args)).to eq(false)
+          it 'resolves to true' do
+            expect(comparator.resolve).to eq(true)
+          end
+        end
+
+        context 'subject is less than object' do
+          let(:subject) { 1 }
+          let(:object) { 2 }
+
+          it 'resolves to false' do
+            expect(comparator.resolve).to eq(false)
+          end
+        end
+
+        context 'when subject is equal to object' do
+          let(:subject) { 1 }
+          let(:object) { 1 }
+
+          it 'resolves to false' do
+            expect(comparator.resolve).to eq(false)
+          end
+        end
       end
     end
   end
