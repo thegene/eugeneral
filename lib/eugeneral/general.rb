841703bf5c58c365da581d10a78aff2acdd1941f
Eugene Westbrook <eugenewestbrook@gmail.com>
2015-09-26 14:05:32 -0400

    General now uses define_singleton_method.
    Conceivably we could just use a bare object, but right now I want to keep an interface for this in case wanna change it.

--- lib/eugeneral/general.rb
+++ lib/eugeneral/general.rb
@@ -2,12 +2,10 @@ module Eugeneral
   class General
 
     def command(command, proc)
-      @command_list ||= {}
-      @command_list[command.to_sym] = proc
+      define_singleton_method(command) do |*args|
+        proc.call(*args)
+      end
     end
 
-    def method_missing(method, *args, &block)
-      @command_list[method].call(*args, &block) if @command_list[method]
-    end
   end
 end
